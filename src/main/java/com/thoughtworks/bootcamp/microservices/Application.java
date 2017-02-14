2693a0b4b9ce76c9132a389651c1ce0cfdf79ff8
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2017-02-14 17:12:00 +0530

    modified config to get spring running

--- src/main/java/com/thoughtworks/bootcamp/microservices/Application.java
+++ src/main/java/com/thoughtworks/bootcamp/microservices/Application.java
@@ -1,4 +1,4 @@
-package com.thoughtworks.bootcamp.microservices.config;
+package com.thoughtworks.bootcamp.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/thoughtworks/bootcamp/microservices/config/SwaggerConfig.java
+++ src/main/java/com/thoughtworks/bootcamp/microservices/config/SwaggerConfig.java
@@ -12,10 +12,13 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 /**
  * Created by abhijeek on 14/02/17.
  */
 @ConfigurationProperties(prefix="swagger")
+@Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
@@ -34,7 +37,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .paths(regex(".*doctors.*"))
                 .build()
                 .pathMapping("/")
                 .apiInfo(apiInfo())
